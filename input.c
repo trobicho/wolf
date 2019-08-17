ad10dd45a4a4c7d91cac07081a9500f20b679b0b
Tom ROBICHON <tomtomy35@gmail.com>
2019-08-17 21:55:10 +0200

    WIP

--- input.c
+++ input.c
@@ -6,7 +6,7 @@
 /*   By: trobicho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/17 16:04:35 by trobicho          #+#    #+#             */
-/*   Updated: 2019/08/17 21:02:32 by trobicho         ###   ########.fr       */
+/*   Updated: 2019/08/17 21:50:26 by trobicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,29 @@ int	user_event(t_wolf *wolf)
 					wolf->player.cam.angle = 0.0;
 			}
 			else if (event.key.keysym.scancode == SDL_SCANCODE_W)
+			{
 				wolf->player.cam.pos.y -= 1;
+				printf("pos (%d, %d)\n", wolf->player.cam.pos.x, wolf->player.cam.pos.y);
+				printf("pos (%d, %d)\n", wolf->player.cam.pos.x / 64, wolf->player.cam.pos.y / 64);
+			}
 			else if (event.key.keysym.scancode == SDL_SCANCODE_S)
+			{
 				wolf->player.cam.pos.y += 1;
+				printf("pos (%d, %d)\n", wolf->player.cam.pos.x, wolf->player.cam.pos.y);
+				printf("pos (%d, %d)\n", wolf->player.cam.pos.x / 64, wolf->player.cam.pos.y / 64);
+			}
 			else if (event.key.keysym.scancode == SDL_SCANCODE_A)
+			{
 				wolf->player.cam.pos.x -= 1;
+				printf("pos (%d, %d)\n", wolf->player.cam.pos.x, wolf->player.cam.pos.y);
+				printf("pos (%d, %d)\n", wolf->player.cam.pos.x / 64, wolf->player.cam.pos.y / 64);
+			}
 			else if (event.key.keysym.scancode == SDL_SCANCODE_D)
+			{
 				wolf->player.cam.pos.x += 1;
+				printf("pos (%d, %d)\n", wolf->player.cam.pos.x, wolf->player.cam.pos.y);
+				printf("pos (%d, %d)\n", wolf->player.cam.pos.x / 64, wolf->player.cam.pos.y / 64);
+			}
 		}
 	}
 	return (0);
